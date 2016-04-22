94c58b57f86d7afb6b6a1cccac99691e25c34b3d
Brad Bootz <fmsminimac1@fmss-mac-mini.cas.honeywell.com>
2016-04-22 16:27:46 -0700

    Commit message

--- Discorgimon/Program.cs
+++ Discorgimon/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +8,7 @@ using System.Collections;
 using System.IO;
 using Newtonsoft.Json;
 using Extensions;
-
+// Brad
 namespace Discorgimon
 {
     public delegate void DiscorgimonOutputCallback(string message);
